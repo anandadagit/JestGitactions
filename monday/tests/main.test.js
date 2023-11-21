1297e4a467f2d5d57c3b73b1a2ab1a8ee6acb3d3
anandadagit <anand@ada.ac.uk>
2023-11-21 16:15:19 +0000

    fixed the error. so github actions should run sucessfully

--- monday/tests/main.test.js
+++ monday/tests/main.test.js
@@ -3,7 +3,7 @@ const main = require('../src/main.js');
 
 describe('my first suite', () => { 
   test('this is my first test', () => {
-    expect(main.sum(2,3)).toBe(4);
+    expect(main.sum(2,3)).toBe(5);
     expect(main.sum(1,1)).toBe(2);
     expect(main.sum(1,1)).not.toBe(3);
   });
